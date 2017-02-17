455af219b4dcad84bb422757a9c99c753b7845d0
Unknown <arjen-k@outlook.com>
2017-02-17 13:03:33 +0800

    Clean project

---
+++ app/src/main/java/kumpul/akses_merakv2/MainActivity.java
@@ -0,0 +1,13 @@
+package kumpul.akses_merakv2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
